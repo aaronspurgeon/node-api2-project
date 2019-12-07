faed812f02634cb790f62cc6d1fbae1a003baad3
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-06 19:54:23 -0500

    Added endpoint to update a post

--- routers/posts.js
+++ routers/posts.js
@@ -67,4 +67,33 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      message: "Invalid post"
+    });
+  }
+
+  const update = {
+    title: req.body.title,
+    contents: req.body.contents
+  };
+
+  db.update(req.params.id, update)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post could not be found"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error updating post"
+      });
+    });
+});
+
 module.exports = router;
