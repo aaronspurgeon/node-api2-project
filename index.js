feb8099338344c92bca2049aa0c39f10ce2b10cc
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-06 19:31:49 -0500

    Created an endpoint to grab all the posts

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const server = express();
+const postRouter = require("./routers/posts");
 
 server.use(express.json());
+
+server.use("/api/posts", postRouter);
 
 server.listen(4000, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n");
